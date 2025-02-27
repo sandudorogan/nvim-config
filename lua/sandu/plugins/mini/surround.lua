ab6ae57d4a338ce29a3e86cfb00ef341302facde
Sandu Dorogan <sandu.dorogan@gmail.com>
2025-02-28 01:12:44 +0200

    fix(plugin): Remove startup warning

--- lua/sandu/plugins/mini/surround.lua
+++
@@ -1,8 +0,0 @@
--- return {
---   "echasnovski/mini.surround",
---   version = false,
---   event = { "BufReadPre", "BufNewFile" },
---   config = function()
---     require("mini.surround").setup()
---   end,
--- }
