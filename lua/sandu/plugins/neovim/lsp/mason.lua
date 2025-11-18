1a367d001be883850f69cde0a6c976d164489553
Sandu Dorogan <sandu.dorogan@gmail.com>
2025-11-18 12:42:48 +0200

    feat(lsp): Further updates after lsp migration

--- lua/sandu/plugins/neovim/lsp/mason.lua
+++ lua/sandu/plugins/neovim/lsp/mason.lua
@@ -1,32 +1,10 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
+  {
     "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
-
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
-
-    local mason_tool_installer = require("mason-tool-installer")
-
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
-
-    mason_lspconfig.setup({
+    opts = {
       -- list of servers for mason to install
       ensure_installed = {
+        "ts_ls",
         "html",
         "cssls",
         "tailwindcss",
@@ -36,12 +14,28 @@ return {
         "emmet_ls",
         "prismals",
         "pyright",
-        "clojure_lsp",
+        "eslint",
       },
-      automatic_installation = true,
-    })
-
-    mason_tool_installer.setup({
+    },
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          ui = {
+            icons = {
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗",
+            },
+          },
+        },
+      },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
       ensure_installed = {
         "prettier", -- prettier formatter
         "stylua", -- lua formatter
@@ -50,6 +44,9 @@ return {
         "pylint",
         "eslint_d",
       },
-    })
-  end,
+    },
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+  },
 }
--- lua/sandu/plugins/neovim/telescope.lua
+++ lua/sandu/plugins/neovim/telescope.lua
@@ -2,38 +2,14 @@ return {
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
   dependencies = {
-    "folke/trouble.nvim",
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown({
-        -- even more opts
-      }),
-
-      -- pseudo code / specification for writing custom displays, like the one
-      -- for "codeactions"
-      -- specific_opts = {
-      --   [kind] = {
-      --     make_indexed = function(items) -> indexed_items, width,
-      --     make_displayer = function(widths) -> displayer
-      --     make_display = function(displayer) -> function(e)
-      --     make_ordinal = function(e) -> string
-      --   },
-      --   -- for example to disable the custom builtin "codeactions" display
-      --      do the following
-      --   codeactions = false,
-      -- }
-    },
   },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
-    local builtin = require("telescope.builtin")
     local transform_mod = require("telescope.actions.mt").transform_mod
 
     local trouble = require("trouble")
@@ -48,7 +24,7 @@ return {
 
     telescope.setup({
       defaults = {
-        path_display = { "filename_first" },
+        path_display = { "smart" },
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
@@ -61,34 +37,15 @@ return {
     })
 
     telescope.load_extension("fzf")
-    telescope.load_extension("ui-select")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>ff", function()
-      builtin.find_files({ hidden = true })
-    end, { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", function()
-      builtin.oldfiles({ hidden = true })
-    end, { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", function()
-      builtin.live_grep({ additional_args = { "--hidden" } })
-    end, { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", function()
-      builtin.grep_string({ hidden = true })
-    end, { desc = "Find string under cursor in cwd" })
-    keymap.set(
-      "n",
-      "<leader>fh",
-      builtin.help_tags,
-      { desc = "Lists available help tags and opens a new window with the relevant help info" }
-    )
-    keymap.set("n", "<leader>fm", builtin.man_pages, { desc = "Lists manpage entries, opens them in a help window" })
-    keymap.set("n", "<leader>fl", builtin.reloader, { desc = "Lists lua modules and reloads them" })
-    keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Lists open buffers" })
-    keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Lists quickfix entries" })
-    keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "Lists tags" })
-    keymap.set("n", "<leader>fo", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+    keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find todos" })
   end,
 }
