c06f9431254b4ce54b021cba21bb3c5035820f64
Sandu Dorogan <sandu.dorogan@gmail.com>
2026-02-25 15:33:57 +0200

    feat: Add render-markdown.nvim
    Pretty-renders markdown in-buffer — headings, code blocks, bullets,
    checkboxes. Also enabled for codecompanion chat buffers.

---
+++ lua/sandu/plugins/neovim/render-markdown.lua
@@ -0,0 +1,36 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  ft = { "markdown", "codecompanion" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    heading = {
+      backgrounds = {
+        "RenderMarkdownH1Bg",
+        "RenderMarkdownH2Bg",
+        "RenderMarkdownH3Bg",
+        "RenderMarkdownH4Bg",
+        "RenderMarkdownH5Bg",
+        "RenderMarkdownH6Bg",
+      },
+      foregrounds = {
+        "RenderMarkdownH1",
+        "RenderMarkdownH2",
+        "RenderMarkdownH3",
+        "RenderMarkdownH4",
+        "RenderMarkdownH5",
+        "RenderMarkdownH6",
+      },
+    },
+    code = {
+      sign = false,
+      width = "block",
+      right_pad = 1,
+    },
+    bullet = {
+      right_pad = 1,
+    },
+  },
+}
